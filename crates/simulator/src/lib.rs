df366622a31a54e354497f5bea1528495908d3a2
clearloop <tianyi.gc@gmail.com>
2025-07-27 19:33:19 +0800

    chore(sim): remove simulator

--- crates/simulator/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! PVM simulator
-
-pub use cmd::App;
-
-mod cmd;
-pub mod ext;
--- services/jam-null-authorizer/src/lib.rs
+++ services/jam-null-authorizer/src/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(any(target_arch = "riscv32", target_arch = "riscv64"), no_std)]
 
 extern crate alloc;
-use alloc::string::String;
 use jam_pvm_common::{is_authorized::*, *};
 use jam_types::*;
 
@@ -10,16 +9,8 @@ struct Authorizer;
 jam_pvm_common::declare_authorizer!(Authorizer);
 
 impl jam_pvm_common::Authorizer for Authorizer {
-	fn is_authorized(param: AuthConfig, package: WorkPackage, core_index: CoreIndex) -> AuthTrace {
-		info!(
-			"Null Authorizer, [{core_index}], {} gas, {param} param, {} token",
-			gas(),
-			package.authorization
-		);
-		if package.authorization.0 != param.0 {
-			panic!("Authorization failed")
-		}
-		let m = String::from_utf8_lossy(&package.authorization);
-		alloc::format!("Auth=<{m}>").as_bytes().to_vec().into()
-	}
+    fn is_authorized(core_index: CoreIndex) -> AuthTrace {
+        info!("Null Authorizer, [{core_index}], {} gas", gas());
+        Default::default()
+    }
 }
