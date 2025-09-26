e5ec529323b57134df1f2dc3cfbf17cd37645cd6
clearloop <tianyi.gc@gmail.com>
2025-09-26 21:02:53 +0800

    chore(services): get back old services

--- crates/sys/build.rs
+++ crates/sys/build.rs
@@ -14,12 +14,12 @@ fn main() -> std::io::Result<()> {
         .status()?;
 
     let lib = out.join("libspacevm.a");
+    fs::rename("libspacevm.a", &lib)?;
     #[cfg(target_os = "linux")]
     if !Command::new("ranlib").arg(&lib).status()?.success() {
         panic!("ranlib failed");
     }
 
-    fs::rename("libspacevm.a", &lib)?;
     println!("cargo:rustc-link-search=native={}", out.display());
     println!("cargo:rustc-link-lib=static=spacevm");
     println!("cargo:rerun-if-changed=build.rs");
